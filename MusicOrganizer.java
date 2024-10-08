7c384cc6b75778861aa6ce782f675ff88bfe4fca
NJWeb.Solutions <david@njweb.solutions>
2024-10-07 23:00:13 -0400

    play 1 random track with method

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -31,7 +31,7 @@ public class MusicOrganizer
         System.out.println("Music library loaded. " + getNumberOfTracks() + " tracks.");
         System.out.println();
     }
-    
+
     /**
      * Add a track file to the collection.
      * @param filename The file name of the track to be added.
@@ -40,7 +40,7 @@ public class MusicOrganizer
     {
         tracks.add(new Track(filename));
     }
-    
+
     /**
      * Add a track to the collection.
      * @param track The track to be added.
@@ -49,7 +49,7 @@ public class MusicOrganizer
     {
         tracks.add(track);
     }
-    
+
     /**
      * Play a track in the collection.
      * @param index The index of the track to be played.
@@ -62,7 +62,7 @@ public class MusicOrganizer
             System.out.println("Now playing: " + track.getArtist() + " - " + track.getTitle());
         }
     }
-    
+
     /**
      * Return the number of tracks in the collection.
      * @return The number of tracks in the collection.
@@ -71,7 +71,7 @@ public class MusicOrganizer
     {
         return tracks.size();
     }
-    
+
     /**
      * List a track from the collection.
      * @param index The index of the track to be listed.
@@ -82,7 +82,7 @@ public class MusicOrganizer
         Track track = tracks.get(index);
         System.out.println(track.getDetails());
     }
-    
+
     /**
      * Show a list of all the tracks in the collection.
      */
@@ -95,7 +95,7 @@ public class MusicOrganizer
         }
         System.out.println();
     }
-    
+
     /**
      * List all tracks by the given artist.
      * @param artist The artist's name.
@@ -108,7 +108,7 @@ public class MusicOrganizer
             }
         }
     }
-    
+
     /**
      * Remove a track from the collection.
      * @param index The index of the track to be removed.
@@ -119,7 +119,7 @@ public class MusicOrganizer
             tracks.remove(index);
         }
     }
-    
+
     /**
      * Play the first track in the collection, if there is one.
      */
@@ -129,7 +129,7 @@ public class MusicOrganizer
             player.startPlaying(tracks.get(0).getFilename());
         }
     }
-    
+
     /**
      * Stop the player.
      */
@@ -149,7 +149,7 @@ public class MusicOrganizer
         // The return value.
         // Set according to whether the index is valid or not.
         boolean valid;
-        
+
         if(index < 0) {
             System.out.println("Index cannot be negative: " + index);
             valid = false;
@@ -163,7 +163,7 @@ public class MusicOrganizer
         }
         return valid;
     }
-    
+
     private void readLibrary(String folderName)
     {
         ArrayList<Track> tempTracks = reader.readTracks(folderName, ".mp3");
@@ -173,21 +173,27 @@ public class MusicOrganizer
             addTrack(track);
         }
     }
-    
-    public void shuffle()
-    {
+
+    /**
+     * Play a single random track from the collection.
+     * This method selects a random track from the list and plays it.
+     * If the collection is empty, a message is printed.
+     */
+    public void playRandomTrack() {
         if (!tracks.isEmpty()) {
-            // Generate a random index between 0 and tracks.size() - 1
-            int randomIndex = random.nextInt(tracks.size() - 1);
+            // Use the existing random object to generate a random index
+            int randomIndex = random.nextInt(tracks.size());
+
             // Retrieve the track at the random index
             Track track = tracks.get(randomIndex);
+
             // Play the selected track
-            player.playSample(track.getFilename());
-            // Print information about the track being played
+            player.startPlaying(track.getFilename());
             System.out.println("Now playing: " + track.getArtist() + " - " + track.getTitle());
         } else {
             // Print a message if there are no tracks to play
             System.out.println("No tracks to play.");
         }
     }
+
 }
